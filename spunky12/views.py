ffc75b87ff9ed62b0adfeed9e0df7f4c7c1d20b4
Tvmbl3r <Tvmbl3r@gmail.com>
2015-07-18 15:33:13 -0700

    My Django Girls app, first commit

---
+++ spunky12/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+	return HttpResponse("dammitall")
+
+def spunky_at_the_park(request):
+	return render(request, "spunky.html")
